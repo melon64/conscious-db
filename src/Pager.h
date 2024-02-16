ac401b43446910d0324f78ebcc4a1bd9f650689d
FelixX <felix.xie@hotmail.com>
2024-02-16 17:53:45 -0500

    added incomplete pager implementation, lots of compiler errors

--- src/Pager.h
+++ src/Pager.h
@@ -13,39 +13,82 @@ using namespace std;
 
 class Pager {
 public:
-    Pager() : file_descriptor_(-1), file_length_(0) {
-        for (size_t i = 0; i < TABLE_MAX_PAGES; i++) {
-            pages_[i] = nullptr;
+    Pager(const std::string& filename) : file_length(0), pages() {
+        file_stream.open(filename, std::ios::in | std::ios::out | std::ios::binary | std::ios::ate);
+        if (!file_stream.is_open()) {
+            std::cerr << "Unable to open file\n";
+            exit(EXIT_FAILURE);
         }
+        file_length = file_stream.tellg();
+        file_stream.seekg(0, std::ios::beg); 
+
+        pages.resize(TABLE_MAX_PAGES);
     }
 
-    ~Pager() {
-        for (size_t i = 0; i < TABLE_MAX_PAGES; i++) {
-            if (pages_[i] != nullptr) {
-                delete[] static_cast<char*>(pages_[i]);
+    char *get_page(size_t page_num) {
+        if (page_num > TABLE_MAX_PAGES) {
+            std::cerr << "Tried to fetch page number out of bounds\n";
+            exit(EXIT_FAILURE);
+        }
+
+        if (!pages[page_num]) {
+            pages[page_num] = std::make_unique<char[]>(PAGE_SIZE);
+            size_t num_pages = file_length / PAGE_SIZE;
+            if (file_length % PAGE_SIZE) {
+                num_pages++;
+            }
+            if (page_num <= num_pages) {
+                file_stream.seekg(page_num * PAGE_SIZE);
+                file_stream.read(pages[page_num].get(), PAGE_SIZE);
+                if (file_stream.bad()) {
+                    std::cerr << "Error reading file\n";
+                    exit(EXIT_FAILURE);
+                }
             }
         }
+
+        return pages[page_num].get();
     }
 
-    void pager_open(string filename){
-        ifstream file;
-        file.open(filename, ios::binary);
-        if (file.is_open()) {
-            file_descriptor_ = file.rdbuf()->pubseekoff(0, ios::cur, ios::in);
-            file.seekg(0, ios::end);
-            file_length_ = file.tellg();
-            file.seekg(0, ios::beg);
-            file.close();
-        } else {
-            cout << "Could not open file\n";
+    void flush(size_t page_num, size_t size){
+        if (!pages[page_num]) {
+            std::cerr << "Tried to flush null page\n";
+            exit(EXIT_FAILURE);
+        }
+
+        file_stream.seekp(page_num * PAGE_SIZE);
+        if (file_stream.bad()) {
+            std::cerr << "Error seeking to page for writing\n";
+            exit(EXIT_FAILURE);
+        }
+        
+        file_stream.write(pages[page_num].get(), size);
+        if (file_stream.bad()) {
+            std::cerr << "Error writing to file\n";
             exit(EXIT_FAILURE);
         }
     }
 
+    int close () {
+        file_stream.close();
+        if (file_stream.bad()) {
+            return 1;
+        }
+        return 0;
+    }
+
+    size_t get_file_length() const {
+        return file_length;
+    }
+
+    vector<unique_ptr<char[]>> get_pages() const {
+        return pages;
+    }
+
 private:
-    int file_descriptor_;
-    size_t file_length_;
-    void* pages_[TABLE_MAX_PAGES];
+    streamoff file_length;
+    std::fstream file_stream;
+    std::vector<std::unique_ptr<char[]>> pages;
 };
 
 #endif
--- src/Table.h
+++ src/Table.h
@@ -3,10 +3,12 @@
 
 #include "Row.h"
 #include "Table.h"
+#include "Pager.h"
 
 #include <iostream>
 #include <cstring>
 #include <vector>
+#include <memory>
 
 static const size_t PAGE_SIZE = 4096;
 static const size_t TABLE_MAX_PAGES = 100;
@@ -15,16 +17,37 @@ static const size_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
 
 class Table {
 public:
-    Table() : num_rows(0) {
-        pages.resize(TABLE_MAX_PAGES, nullptr);
+     Table() : num_rows(0), pager(nullptr) {}    
+
+    void db_open(const std::string& filename) {
+        pager = std::make_unique<Pager>(filename);
+        num_rows = pager->get_file_length() / sizeof(Row);
     }
 
-    ~Table() {
-        for (auto page : pages) {
-            if (page != nullptr)    {
-                delete[] static_cast<char*>(page);
+    void db_close(){
+        size_t num_full_pages = num_rows / ROWS_PER_PAGE;
+        for (size_t i = 0; i < num_full_pages; i++) {
+            if (!pager->get_pages()[i]) {
+                continue;
+            }
+            pager->flush(i, PAGE_SIZE);
+            pager->get_pages()[i].reset();
+        }
+
+        size_t num_additional_rows = num_rows % ROWS_PER_PAGE;
+        if (num_additional_rows > 0) {
+            size_t page_num = num_full_pages;
+            if (pager->get_pages()[page_num]) {
+                pager->flush(page_num, num_additional_rows * sizeof(Row));
+                pager->get_pages()[page_num].reset();
             }
         }
+
+        if (pager->close()) {
+            std::cerr << "Error closing db file\n";
+            exit(EXIT_FAILURE);
+        }
+        pager.reset();
     }
 
     bool insert(const Row& row) {
@@ -54,16 +77,14 @@ public:
 
 private:
     size_t num_rows;
-    std::vector<void*> pages;
+    std::unique_ptr<Pager> pager;
 
     void* row_slot(size_t row_num) {
         size_t page_num = row_num / ROWS_PER_PAGE;
-        if (!pages[page_num]) {
-            pages[page_num] = new char[PAGE_SIZE];
-        }
+        char* page = pager->get_page(page_num);
         size_t row_offset = row_num % ROWS_PER_PAGE;
         size_t byte_offset = row_offset * sizeof(Row);
-        return static_cast<char*>(pages[page_num]) + byte_offset;
+        return page + byte_offset;
     }
 };
 
